6525d00634c38816d79e962bd5317f24f0053910
Nick Martinez <23nicholasmartinez@gmail.com>
2017-01-18 07:42:38 -0800

    Set up testing library

---
+++ tinyTester.js
@@ -0,0 +1,17 @@
+var TinyTester = {
+
+  run: function(tests) {
+    var successes = 0;
+    var failures = 0;
+
+    for (var testName in tests) {
+      var testAction = tests[testName];
+
+      try {
+        catch (e) {
+
+        }
+      }
+    }
+  }
+};
