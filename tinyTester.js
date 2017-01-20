ef33d9f80fa5297c0da097d53d9be584545b1142
Nick Martinez <23nicholasmartinez@gmail.com>
2017-01-20 08:33:16 -0800

    Add libraryLoader file and Try Catch error handling

--- tinyTester.js
+++ tinyTester.js
@@ -1,16 +1,19 @@
 var TinyTester = {
-
+  // testName is set to the property in each iteration.
+  // Try - Catch will apply and run TinyTester or return an error stack.
   run: function(tests) {
-    var successes = 0;
     var failures = 0;
 
     for (var testName in tests) {
       var testAction = tests[testName];
-
       try {
-        catch (e) {
-
-        }
+        testAction.apply(this);
+        console.log('%c' + testName, "color: green;");
+      } catch (e) {
+        failures++;
+        console.groupCollapsed('%c' + testName, "color: red;");
+        console.error(e.stack);
+        console.groupEnd();
       }
     }
   }
