bb931ada96adbae39dc64796baab70876991ae52
Nick Martinez <23nicholasmartinez@gmail.com>
2017-01-23 10:20:09 -0800

    Add test helper and setTimeout for DOM background

--- tinyTester.js
+++ tinyTester.js
@@ -1,3 +1,17 @@
+var TinyTestHelper = {
+  renderStats: function(tests, failures) {
+    var numberOfTests = Object.keys(tests).length;
+    var successes = numberOfTests - failures;
+    var summaryString = 'Ran ' + numberOfTests + ' tests: ' + successes +
+                        ' successes, ' + failures + ' failures';
+
+    var summaryElement = document.createElement('h1');
+    summaryElement.textContent = summaryString;
+    document.body.appendChild(summaryElement);
+  }
+};
+
+
 var TinyTester = {
   // testName is set to the property in each iteration.
   // Try - Catch will apply and run TinyTester or return an error stack.
@@ -16,5 +30,12 @@ var TinyTester = {
         console.groupEnd();
       }
     }
+    // setTimeout webAPI will allow the DOM to update rendering before callback runs.
+    setTimeout(function() {
+      if (window.document && document.body) {
+        document.body.style.backgroundColor = (failures == 0 ? '#99ff99' : '#ff9999');
+        TinyTestHelper.renderStats(tests, failures);
+      }
+    }, 0);
   }
 };
